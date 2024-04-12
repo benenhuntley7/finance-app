29400b981242d1d39cb802d62bf0b3a518a2f9fd
Benen Huntley <benen.huntley@gmail.com>
2024-04-12 20:04:30 +0930

    table scroll for small screens

--- src/app/shares/[symbol]/page.tsx
+++ src/app/shares/[symbol]/page.tsx
@@ -23,52 +23,54 @@ export default async function Page({ params }: { params: { symbol: string } }) {
 
   return (
     <main className="flex flex-col items-center justify-between relative px-2 lg:px-20">
-      <div className="flex flex-col w-full md:w-1/2 justify-center  overflow-x-scroll md:overflow-auto">
+      <div className="flex flex-col w-full md:w-1/2 justify-center">
         {currentData ? (
           <>
             <h1 className="flex w-full font-bold my-5 justify-center">
               {currentData.symbol} - {currentData.longName}
             </h1>
             {holdings && holdings.length ? (
-              <table className="table table-zebra table-sm">
-                <thead className="border-b border-black border-3">
-                  <tr>
-                    <th>Purchase Date</th>
-                    <th>Purchase Price</th>
-                    <th>Brokerage</th>
-                    <th>Quantity</th>
-                    <th>Current Value</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {/* Map over holdings and render table rows */}
-                  {holdings.map((holding, index) => (
-                    <tr key={index}>
-                      {/* Render data in table cells */}
-                      <td>{holding.purchase_date?.toLocaleDateString()}</td>
-                      <td>${holding.purchase_price}</td>
-                      <td>${holding.brokerage?.toFixed(2)}</td>
-                      <td>{holding.qty}</td>
-                      <td>${(currentData!.regularMarketPrice! * holding.qty!).toFixed(2)}</td>
+              <div className="  overflow-x-scroll md:overflow-auto">
+                <table className="table table-zebra table-sm">
+                  <thead className="border-b border-black border-3">
+                    <tr>
+                      <th>Purchase Date</th>
+                      <th>Purchase Price</th>
+                      <th>Brokerage</th>
+                      <th>Quantity</th>
+                      <th>Current Value</th>
                     </tr>
-                  ))}
-                </tbody>
-                <tfoot className="border-t border-black border-3">
-                  <tr>
-                    <td>Total</td>
-                    <td></td>
-                    <td>${holdings.reduce((total, purchase) => total + purchase.brokerage!, 0).toFixed(2)}</td>
-                    <td>${holdings.reduce((total, purchase) => total + purchase.qty!, 0)}</td>
-                    <td>
-                      $
-                      {holdings.reduce(
-                        (total, purchase) => total + currentData!.regularMarketPrice! * purchase.qty!,
-                        0
-                      )}
-                    </td>
-                  </tr>
-                </tfoot>
-              </table>
+                  </thead>
+                  <tbody>
+                    {/* Map over holdings and render table rows */}
+                    {holdings.map((holding, index) => (
+                      <tr key={index}>
+                        {/* Render data in table cells */}
+                        <td>{holding.purchase_date?.toLocaleDateString()}</td>
+                        <td>${holding.purchase_price}</td>
+                        <td>${holding.brokerage?.toFixed(2)}</td>
+                        <td>{holding.qty}</td>
+                        <td>${(currentData!.regularMarketPrice! * holding.qty!).toFixed(2)}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                  <tfoot className="border-t border-black border-3">
+                    <tr>
+                      <td>Total</td>
+                      <td></td>
+                      <td>${holdings.reduce((total, purchase) => total + purchase.brokerage!, 0).toFixed(2)}</td>
+                      <td>${holdings.reduce((total, purchase) => total + purchase.qty!, 0)}</td>
+                      <td>
+                        $
+                        {holdings.reduce(
+                          (total, purchase) => total + currentData!.regularMarketPrice! * purchase.qty!,
+                          0
+                        )}
+                      </td>
+                    </tr>
+                  </tfoot>
+                </table>
+              </div>
             ) : (
               <div className="w-full justify-center text-center">
                 You do not have any holdings in this company. Add a holding{" "}
