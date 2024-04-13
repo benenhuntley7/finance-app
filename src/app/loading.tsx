b2e937d60985e2a5128c5f6c0ed97a2f1dc344f5
Benen Huntley <benen.huntley@gmail.com>
2024-04-13 11:03:47 +0930

    loading spinner size

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="flex justify-center items-center">
       {/* Parent container with flex and centering properties */}
-      <div className="loading loading-spinner w-20 h-20 max-w-20 max-h-20"></div>
+      <div className="loading loading-spinner w-15 h-15 max-w-15 max-h-15"></div>
     </div>
   );
 }
